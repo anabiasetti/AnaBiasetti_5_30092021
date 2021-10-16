df43d6549c1e90bca468577fa8afd493f3906cdc
Ana Biasetti <anabiasetti@protonmail.com>
2021-10-16 21:07:45 +0200

    functions pour modifier quantité et suprimmer

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,9 +1,11 @@
-// function displayProduct afin d'afficher les details pour chaque produit ajouté au panier
+// Function displayProduct afin d'afficher les details pour chaque produit ajouté au panier
 function displayProduct(content, details) {
   //<article>
   const sectionItems = document.getElementById("cart__items");
   const article = document.createElement("article");
   article.classList.add("cart__item");
+  article.setAttribute("data-id", content.id);
+  article.setAttribute("data-color", content.color);
   sectionItems.appendChild(article);
   // Image
   const divImg = document.createElement("div");
@@ -43,6 +45,7 @@ function displayProduct(content, details) {
   inputQuantity.name = "itemQuantity";
   inputQuantity.min = "1";
   inputQuantity.max = "100";
+  inputQuantity.addEventListener("change", changeQuantity);
   divQuantity.appendChild(inputQuantity);
   divSettings.appendChild(divQuantity);
   divContent.appendChild(divSettings);
@@ -51,25 +54,35 @@ function displayProduct(content, details) {
   const divDelete = document.createElement("div");
   divDelete.classList.add("cart__item__content__settings__delete");
   const pDelete = document.createElement("p");
+
+  //----------------gestion du buton pour supprimer le produit------------------------//
   pDelete.classList.add("deleteItem");
   pDelete.textContent = "Supprimer";
-  pDelete.classList.add("deleteItem");
   divDelete.appendChild(pDelete);
   divSettings.appendChild(divDelete);
+  pDelete.addEventListener("click", deleteItem);
 }
 
-//function pour afficher le contenu du panier dans la page panier
+/**
+ * function pour afficher le contenu du panier dans la page panier
+ */
 function displayCartContents() {
   // Récupérer les données stockées dans le pannier
   const stringifiedValueFromLocalStorage = localStorage.getItem("cart");
   let cart;
   if (stringifiedValueFromLocalStorage) {
+    console.log("Panier a du contenu, parser l'array du localstorage");
     cart = JSON.parse(stringifiedValueFromLocalStorage);
   } else {
+    console.log("Panier est vide, initialiser à un array vide");
     cart = [];
   }
-  //parcourir l'array
-  //const parce que on dois garder le contenu pour que le callback de fetch l'utilise plus tard
+  //Parcourir l'array
+  //const parce que on doit garder le contenu pour que le callback de fetch l'utilise plus tard
+  /**
+   * for (let i= 0;i<cart.length;i++) {
+   * const content = cart[i];
+   */
   for (const content of cart) {
     console.log(content);
     fetch("http://localhost:3000/api/products/" + content.id)
@@ -89,7 +102,43 @@ function displayCartContents() {
   }
 }
 
-//----------------gestion du buton pour supprimer le produit------------------------//
+//function changeQuantity pour modifier la quantité
+
+function changeQuantity(event) {
+  // Recuperer id, color pour celui ou on a cliqué
+  const id = event.target.closest("article").getAttribute("data-id");
+  const color = event.target.closest("article").getAttribute("data-color");
+  console.log(color, id);
+
+  // Reprendre cart depuis localstorage
+
+  const cart = getCart();
+  // Trouver le produit dans cart et changer son quantité
+
+  for (const content of cart) {
+    if (content.id === id && content.color === color) {
+      content.quantity = event.target.value;
+    }
+  }
+
+  // sauvegarder dans localstorage le nouveau cart
+  storeCart(cart);
+}
+
+function deleteItem(event) {
+  const id = event.target.closest("article").getAttribute("data-id");
+  const color = event.target.closest("article").getAttribute("data-color");
+  // Reprendre cart depuis localstorage
+
+  let cart = getCart();
+  // Trouver le produit dans cart et supprimer
+
+  cart = cart.filter((content) => !(content.id === id && content.color === color));
+
+  // Sauvegarder dans localstorage le nouveau cart
+  storeCart(cart);
+}
+
 //-------------------form pour commander------------------------------------------//
 function submit() {
   const questions = document.getElementsByClassName("cart__order__form__question");
